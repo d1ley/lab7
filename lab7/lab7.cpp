cc452c8d17f77d590814862dee360bdff25638cd
arowp <arowp@DILEY>
2023-06-15 00:20:54 +0200

    Добавьте файлы проекта.

---
+++ lab7/lab7.cpp
@@ -0,0 +1,18 @@
+﻿#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <limits>
+#include <vector>
+
+#include "game.h"
+
+using namespace std;
+
+int main() {
+	srand(time(0));
+
+	Game game;
+	game.start();
+
+	return 0;
+}
